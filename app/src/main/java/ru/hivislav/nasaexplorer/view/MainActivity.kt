2e8e5c93f60a28aa4385d21e885d98273c471381
hivislav <hivislav@gmail.com>
2022-07-04 20:56:42 +0500

    task 2.1
    // реализовал переключение тем и сохранение их в sharedPref

--- app/src/main/java/ru/hivislav/nasaexplorer/view/MainActivity.kt
+++ app/src/main/java/ru/hivislav/nasaexplorer/view/MainActivity.kt
@@ -1,16 +1,56 @@
 package ru.hivislav.nasaexplorer.view
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import ru.hivislav.nasaexplorer.R
+import ru.hivislav.nasaexplorer.databinding.ActivityMainBinding
+
+const val ThemeDefault = 0
+const val ThemeRed = 1
+const val ThemeSilverGold = 2
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val KEY_SP = "sp"
+    private val KEY_CURRENT_THEME = "current_theme"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setTheme(applyAppStyle(getCurrentTheme()))
+        binding =  ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction().add(R.id.container, MainFragment.newInstance()).commit()
+        }
+    }
+
+    fun setCurrentTheme(currentTheme: Int) {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt(KEY_CURRENT_THEME, currentTheme)
+        editor.apply()
+    }
+
+    fun getCurrentTheme(): Int {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_CURRENT_THEME, -1)
+    }
+
+    private fun applyAppStyle(currentTheme: Int): Int {
+        return when (currentTheme) {
+            ThemeDefault -> R.style.Theme_NASAExplorer
+            ThemeRed -> R.style.Theme_NASAExplorerRed
+            ThemeSilverGold -> R.style.Theme_NASAExplorerSilverGold
+            else -> 0
         }
     }
 }
--- app/src/main/java/ru/hivislav/nasaexplorer/view/SettingsFragment.kt
+++ app/src/main/java/ru/hivislav/nasaexplorer/view/SettingsFragment.kt
@@ -1,23 +1,74 @@
 package ru.hivislav.nasaexplorer.view
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 import ru.hivislav.nasaexplorer.R
+import ru.hivislav.nasaexplorer.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var parentActivity: MainActivity
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        parentActivity = requireActivity() as MainActivity
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setTabLayoutClicks()
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when(tab?.position) {
+                    ThemeDefault -> {parentActivity.setCurrentTheme(ThemeDefault)
+                        parentActivity.recreate()}
+                    ThemeRed -> {parentActivity.setCurrentTheme(ThemeRed)
+                        parentActivity.recreate()}
+                    ThemeSilverGold -> { parentActivity.setCurrentTheme(ThemeSilverGold)
+                        parentActivity.recreate()
+                    }
+                }
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
+    }
+
+    private fun setTabLayoutClicks() = with(binding) {
+        when(parentActivity.getCurrentTheme()) {
+            ThemeDefault -> {
+                tabLayout.selectTab(tabLayout.getTabAt(ThemeDefault))
+            }
+            ThemeRed -> {
+                tabLayout.selectTab(tabLayout.getTabAt(ThemeRed))
+            }
+            ThemeSilverGold -> {
+                tabLayout.selectTab(tabLayout.getTabAt(ThemeSilverGold))
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
     companion object {
